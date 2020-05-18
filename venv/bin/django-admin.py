fa7298b3a8363c5386b79e57ee1ee0935174e143
phelloochi <phelixabila@gmail.com>
2020-05-18 16:29:21 +0300

    added

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/phelloh/Desktop/Dev/pythonapps/djangoapps/instagram/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
